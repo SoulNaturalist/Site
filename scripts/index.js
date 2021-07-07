ca7457bf4d461b426e1040b9a8511c4541e75483
Saywex89 <omg12121@mail.ru>
2021-07-07 11:49:17 +0700

    add check time

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+const now = new Date();
+
+
+let current_time = now.getHours()+':'+now.getMinutes()+':'+now.getSeconds();
+
+let thisHour = now.getHours();
+
+
+if (Number(thisHour) < 18){
+    window.location.replace('./TestVersionDay/index.html')
+}
