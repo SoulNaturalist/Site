133608941dc50550ebdf9e1178b9e962acfc0618
Saywex89 <omg12121@mail.ru>
2021-07-07 13:38:10 +0700

    comment js code

---
+++ TestVersionDay/scripts/index.js
@@ -0,0 +1,24 @@
+// function Update_page_state() {
+//     state_site = true;
+
+// }
+
+// const now = new Date();
+
+// const thisHour = now.getHours();
+
+
+// if (Number(thisHour) < 18 && state_site !== true){
+//     if (document.URL.indexOf('TestVersionDay') === -1 && state_site !== true){
+//         console.log(1, state_site)
+//         window.location.replace('TestVersionDay/index.html')
+//     }
+// }
+
+// else if (Number(thisHour) > 18 && state_site !== true) {
+//     console.log(2)
+//     if (document.URL.indexOf('Site_card/index.html') !== -1){
+//         console.log(3)
+//         window.location.replace('../index.html')
+//     }
+// }
--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,27 @@
-const now = new Date();
+localStorage.setItem('state', 'false')
 
 
-let current_time = now.getHours()+':'+now.getMinutes()+':'+now.getSeconds();
+function Update_page_state() {
+    state_site = true;
 
-let thisHour = now.getHours();
-
-
-if (Number(thisHour) < 18){
-    window.location.replace('./TestVersionDay/index.html')
 }
 
-else if (Number(thisHour) > 18 || Number(thisHour) === 18) {
-    window.location.replace('index.html')
+const now = new Date();
+
+const thisHour = now.getHours();
 
 
+if (Number(thisHour) < 18 && state_site !== true){
+    if (document.URL.indexOf('TestVersionDay') === -1 && state_site !== true){
+        console.log(1, state_site)
+        window.location.replace('TestVersionDay/index.html')
+    }
+}
+
+else if (Number(thisHour) > 18 && state_site !== true) {
+    console.log(2)
+    if (document.URL.indexOf('Site_card/index.html') !== -1){
+        console.log(3)
+        window.location.replace('../index.html')
+    }
 }
